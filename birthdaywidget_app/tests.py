e24bc47ca345af505e5dd3f881fc3e645d5e3df9
Buhle Ntshalintshali <buhlezwide@yahoo.com>
2016-05-26 15:22:09 +0200

    fixing tests

--- birthdaywidget_app/tests.py
+++ birthdaywidget_app/tests.py
@@ -8,62 +8,71 @@ from birthdaywidget import settings
 class BirthdayWidgetTestCase(TestCase):
 
     def setUp(self):
-        self.username= 'buhlen'
+        pass
 
     def testListingTodayBirthdays(self):
         """>>>test listing friend who are birthdaying today"""
+        username = 'buhlen'
 
-        APIRequestFactory().get('/api/v1/birthdays/buhlen/')
+        APIRequestFactory().get('/api/v1/birthdays/' + username + '/')
 
-        request1 	= APIRequestFactory().get("/api/v1/birthdays/buhlen/")
+        request1 	= APIRequestFactory().get('/api/v1/birthdays/' + username + '/')
         get_detail  = BirthdayListViewSet.as_view({'get': 'list'})
 
-        response1 	= get_detail(request1)
+        response1 	= get_detail(request1,username=username)
 
         self.assertEqual(response1.status_code,status.HTTP_200_OK)
 
     def testListingMonthBirthdays(self):
         """>>>test listing friend who are birthdaying on the specific month"""
+        username = 'buhlen'
+        month = 1
 
-        APIRequestFactory().get('/api/v1/birthdays/buhlen/1/')
+        APIRequestFactory().get('/api/v1/birthdays/'+username +'/'+ str(month)+'/')
 
-        request1 	= APIRequestFactory().get("/api/v1/birthdays/buhlen/1/")
+        request1 	= APIRequestFactory().get('/api/v1/birthdays/'+ username +'/'+ str(month)+'/')
         get_detail  = MonthBirthdayListViewSet.as_view({'get': 'list'})
 
-        response1 	= get_detail(request1)
+        response1 	= get_detail(request1,username=username, month=unicode(month))
 
         self.assertEqual(response1.status_code,status.HTTP_200_OK)
 
     def testInvalidMonthBirthdays(self):
         """>>>test use wrong month"""
+        username = 'buhlen'
+        month = 13
 
-        APIRequestFactory().get('/api/v1/birthdays/buhlen/13/')
+        APIRequestFactory().get('/api/v1/birthdays/'+ username +'/'+ str(month)+'/')
 
-        request1 	= APIRequestFactory().get("/api/v1/birthdays/buhlen/13/")
+        request1 	= APIRequestFactory().get('/api/v1/birthdays/'+ username +'/'+ str(month)+'/')
         get_detail  = MonthBirthdayListViewSet.as_view({'get': 'list'})
 
-        response1 	= get_detail(request1)
+        response1 	= get_detail(request1,username=username, month=unicode(month))
 
         self.assertEqual(response1.status_code,status.HTTP_400_BAD_REQUEST)
 
     def testInvalidMonth(self):
         """>>>test use 3 digits for month"""
+        username = 'buhlen'
+        month= 123
+        APIRequestFactory().get('/api/v1/birthdays/'+ username +'/'+ str(month)+'/')
 
-        APIRequestFactory().get('/api/v1/birthdays/buhlen/133/')
-
-        request1 	= APIRequestFactory().get("/api/v1/birthdays/buhlen/133/")
+        request1 	= APIRequestFactory().get('/api/v1/birthdays/'+ username +'/'+ str(month)+'/')
         get_detail  = MonthBirthdayListViewSet.as_view({'get': 'list'})
 
-        response1 	= get_detail(request1)
+        response1 	= get_detail(request1,username=username, month=unicode(month))
 
         self.assertEqual(response1.status_code,status.HTTP_400_BAD_REQUEST)
 
     def testListingDateBirthdays(self):
         """>>>test listing people who are birthdaying on the specific date"""
+        username = 'phumi1'
+        month = 10
+        day = 1
 
-        request1 	= APIRequestFactory().get('/api/v1/birthdays/buhlen/'+str(10)+'/'+str(1)+'/')
+        request1 	= APIRequestFactory().get('/api/v1/birthdays/'+ username +'/10/1/')
         get_detail  = DateBirthdayListViewSet.as_view({'get': 'list'})
-        response1 	= get_detail(request1)
+        response1 	= get_detail(request1,username=username,month=unicode(month),day=unicode(day))
 
         self.assertEqual(response1.status_code,status.HTTP_200_OK)
 
